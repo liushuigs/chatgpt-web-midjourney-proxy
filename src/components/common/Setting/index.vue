059e270badb86b5b02e7642bfcdde28db6de2c67
jinlingxi <jinlingxi@gmail.com>
2024-03-13 21:09:16 +0800

    hide about tab

--- src/components/common/Setting/index.vue
+++ src/components/common/Setting/index.vue
@@ -2,7 +2,6 @@
 import { computed, ref } from 'vue'
 import { NModal, NTabPane, NTabs } from 'naive-ui'
 import General from './General.vue'
-import About from './About.vue'
 import aiModel from '@/views/mj/aiModel.vue'
 import aiSetServer from '@/views/mj/aiSetServer.vue'
 import { homeStore, useAuthStore } from '@/store'
@@ -68,14 +67,14 @@ const show = computed({
           </template>
           <aiSetServer />
         </NTabPane>
-        <NTabPane name="Config" tab="Config">
+        <!-- <NTabPane name="Config" tab="Config">
           <template #tab>
             <SvgIcon class="text-lg" icon="ri:list-settings-line" />
-            <!-- <span class="ml-2">{{ $t('setting.config') }}</span> -->
+            <span class="ml-2">{{ $t('setting.config') }}</span>
             <span class="ml-2">{{ $t('mjset.about') }}</span>
           </template>
           <About />
-        </NTabPane>
+        </NTabPane> -->
       </NTabs>
     </div>
   </NModal>
