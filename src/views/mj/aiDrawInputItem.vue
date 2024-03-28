81b44174c88ed0a67a1324cdf24c17c5cd2dbe29
Dooy <dooy520@qq.com>
2024-03-28 17:04:53 +0800

    cw

--- src/views/mj/aiDrawInputItem.vue
+++ src/views/mj/aiDrawInputItem.vue
@@ -24,7 +24,7 @@ const vf = [{ s: 'width: 100%; height: 100%;', label: '1:1' },
   { s: 'width: 50%; height: 100%;', label: '9:16' },
 ]
 
-const f = ref({ bili: -1, quality: '', view: '', light: '', shot: '', style: '', styles: '', version: '--v 6.0',cw:0,sref:'',cref:"" })
+const f = ref({ bili: -1, quality: '', view: '', light: '', shot: '', style: '', styles: '', version: '--v 6.0',cw:'',sref:'',cref:"" })
 const st = ref({
   text: '',
   isDisabled: false,
@@ -133,7 +133,7 @@ function createPrompt(rz: string) {
   mlog('createPrompt ', rz, f.value)
   if( f.value.sref.trim() != '' ) rzp += ` --sref ${f.value.sref}`
   if( f.value.cref.trim() != '' ) rzp += ` --cref ${f.value.cref}`
-  if( f.value.cw!=null && f.value.cw>=0 && f.value.cw<=100  ) rzp += ` --cw ${f.value.cw}`
+  if( f.value.cw!=null && f.value.cw!='' && f.value.cw>=0 && f.value.cw<=100  ) rzp += ` --cw ${f.value.cw}`
   
   if (f.value.bili > -1) rzp += ` --ar ${vf[f.value.bili].label}`
   rz = rzk + rz + rzp
